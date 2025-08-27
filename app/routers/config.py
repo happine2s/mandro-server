ad768171a9c108ea4c5bc4f94073caf307cab79c
happine2s <happine2s@sookmyung.ac.kr>
2025-08-26 20:15:08 -0900

    Feat: /config API 해상도, 화질, FPS 포함하여 확장

--- app/routers/config.py
+++ app/routers/config.py
@@ -18,10 +18,19 @@ def update_config(
 
     cam1_order: int = Form(...),
     cam1_rotation: int = Form(...),
-    cam1_flipped: bool = Form(...)
+    cam1_flipped: bool = Form(...),
+
+    stream_width: int = Form(...),
+    stream_height: int = Form(...),
+    stream_quality: int = Form(...),
+    stream_fps: int = Form(...)
 ):
     cam0 = {"order": cam0_order, "rotation": cam0_rotation, "flipped": cam0_flipped}
     cam1 = {"order": cam1_order, "rotation": cam1_rotation, "flipped": cam1_flipped}
-
-    new_config = config_service.update_config(gap, cam0, cam1)
+    stream = {
+        "resolution": [stream_width, stream_height],
+        "quality": stream_quality,
+        "fps": stream_fps
+    }
+    new_config = config_service.update_config(gap, cam0, cam1, stream)
     return {"message": "카메라 설정이 수정되었습니다.", "config": new_config}
